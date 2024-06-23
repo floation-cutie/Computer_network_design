237239fb4f7be527d5f59d64f7c9c221a4a14cb8
floation_cutie <2185368154@qq.com>
2024-06-23 16:44:59 +0800

    to test multiple host

--- test/test_socket.c
+++ test/test_socket.c
@@ -1,84 +1,71 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <winsock2.h>
 
-#define BUFFER_SIZE 512
+#pragma comment(lib, "ws2_32.lib")
 
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
+#define DNS_PORT 53
+
+typedef struct {
+    SOCKET sockfd;
+    struct sockaddr_in addr;
+} DNS_Server;
+
+void cleanup_socket(DNS_Server *server) {
+    closesocket(server->sockfd);
+    WSACleanup();
 }
 
 int main() {
-    int sockfd;
-    struct sockaddr_in server_addr, client_addr, forward_addr;
-    socklen_t addr_len = sizeof(struct sockaddr_in);
-    unsigned char buffer[BUFFER_SIZE];
-    ssize_t recv_len, sent_len;
+    WSADATA wsa;
+    DNS_Server rs;
+    int recv_len;
+    char buf[512];
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
 
-    // 配置监听的 IP 和端口
-    const char *LISTEN_IP = "0.0.0.0";
-    const int LISTEN_PORT = 5353;
-
-    // 配置转发的 DNS 服务器 IP 和端口
-    const char *FORWARD_IP = "8.8.8.8";
-    const int FORWARD_PORT = 53;
-
-    // 创建 socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        error("Failed to create socket");
+    // 初始化 Winsock
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed to initialize Winsock. Error Code: %d\n", WSAGetLastError());
+        return EXIT_FAILURE;
     }
 
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(LISTEN_IP);
-    server_addr.sin_port = htons(LISTEN_PORT);
-
-    // 绑定 socket 到指定的 IP 和端口
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        error("Failed to bind socket");
+    // 创建套接字
+    if ((rs.sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET) {
+        printf("Could not create socket. Error Code: %d\n", WSAGetLastError());
+        WSACleanup();
+        return EXIT_FAILURE;
     }
 
-    printf("DNS server listening on %s:%d\n", LISTEN_IP, LISTEN_PORT);
+    // 配置地址结构
+    rs.addr.sin_family = AF_INET;
+    rs.addr.sin_addr.s_addr = INADDR_ANY;
+    rs.addr.sin_port = htons(DNS_PORT);
 
+    // 绑定套接字
+    if (bind(rs.sockfd, (struct sockaddr *)&rs.addr, sizeof(rs.addr)) == SOCKET_ERROR) {
+        printf("Bind failed. Error Code: %d\n", WSAGetLastError());
+        cleanup_socket(&rs);
+        return EXIT_FAILURE;
+    }
+
+    printf("DNS Server is listening on port %d...\n", DNS_PORT);
+
+    // 循环接收数据
     while (1) {
-        // 接收来自客户端的 DNS 查询
-        recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &addr_len);
-        if (recv_len < 0) {
-            error("Failed to receive data");
+        memset(buf, 0, sizeof(buf));
+        recv_len = recvfrom(rs.sockfd, buf, sizeof(buf), 0, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (recv_len == SOCKET_ERROR) {
+            printf("recvfrom() failed. Error Code: %d\n", WSAGetLastError());
+            continue;
         }
 
-        printf("Received DNS query from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        printf("Received DNS request from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
-        // 配置转发地址
-        memset(&forward_addr, 0, sizeof(forward_addr));
-        forward_addr.sin_family = AF_INET;
-        forward_addr.sin_addr.s_addr = inet_addr(FORWARD_IP);
-        forward_addr.sin_port = htons(FORWARD_PORT);
-
-        // 转发 DNS 查询到实际的 DNS 服务器
-        sent_len = sendto(sockfd, buffer, recv_len, 0, (struct sockaddr *)&forward_addr, sizeof(forward_addr));
-        if (sent_len < 0) {
-            error("Failed to forward data");
-        }
-
-        // 接收来自 DNS 服务器的响应
-        recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
-        if (recv_len < 0) {
-            error("Failed to receive response");
-        }
-
-        // 将响应发送回原始客户端
-        sent_len = sendto(sockfd, buffer, recv_len, 0, (struct sockaddr *)&client_addr, addr_len);
-        if (sent_len < 0) {
-            error("Failed to send response");
-        }
-
-        printf("Sent DNS response to %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        // 这里可以处理接收到的 DNS 请求并发送响应
     }
 
-    close(sockfd);
-    return 0;
+    cleanup_socket(&rs);
+    return EXIT_SUCCESS;
 }
